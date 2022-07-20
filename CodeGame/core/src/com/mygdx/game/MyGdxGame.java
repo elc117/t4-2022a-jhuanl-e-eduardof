176624391123a0d3f8337256e3d1d5fc302ad1a8
EduFritzen <93085789+EduFritzen@users.noreply.github.com>
2022-07-20 15:10:42 -0300

    Update MyGdxGame.java

--- CodeGame/core/src/com/mygdx/game/MyGdxGame.java
+++ CodeGame/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,45 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("Sample.png");
-	}
+   SpriteBatch batch;
+   Texture img;
+   TextureRegion[] animationFrames;
+   Animation<TextureRegion> animation;
+   float elapsedTime;
+   
+   @Override
+   public void create () {
+      batch = new SpriteBatch();
+      img = new Texture("character_pose.png");
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+      TextureRegion[][] tmpFrames = TextureRegion.split(img,1728,1280);
+
+      animationFrames = new TextureRegion[45];
+      int index = 0;
+
+      for (int i = 0; i < 5; i++){
+         for(int j = 0; j < 9; j++) {
+            animationFrames[index++] = tmpFrames[j][i];
+         }
+      }
+
+      animation = new Animation<TextureRegion>(1f/15f,animationFrames);
+   }
+
+   @Override
+   public void render () {
+      elapsedTime += Gdx.graphics.getDeltaTime();
+      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+      batch.begin();
+      batch.draw(animation.getKeyFrame(elapsedTime,true),0,0);
+      batch.end();
+   }
 }
